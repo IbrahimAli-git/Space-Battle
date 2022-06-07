5ea9a589ae1bd9b01a5f1c6bfa92124e8f44c5d2
Ibrahim Ali <ibrahim2ali169@gmail.com>
2022-06-07 08:18:28 +0100

    Update BaseObject.java

--- BaseObject.java
+++ BaseObject.java
@@ -1,18 +1,24 @@
 package com.codegym.task.task25.task2515;
 
+/**
+ * The base class for all game objects.
+ */
 public abstract class BaseObject {
-    private double x;
-    private double y;
-    private double radius;
+    // Coordinates
+    protected double x;
+    protected double y;
+    // Object radius
+    protected double radius;
+    // Indicates whether the object is alive
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        isAlive = true;
+        this.isAlive = true;
     }
-    
+
     public double getX() {
         return x;
     }
@@ -37,23 +43,46 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
+    /**
+     * This method draws the object on the canvas.
+     */
+    public void draw(Canvas canvas) {
+        // Do nothing
+    }
+
+    /**
+     * Move by one step.
+     */
+    public void move() {
+        // Do nothing
+    }
+
+    /**
+     * Check whether (x, y) is beyond the borders.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
     public boolean isAlive() {
         return isAlive;
     }
 
-    public void draw(){
-
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
-    public void move(){
-
-    }
-
-    public void die(){
+    public void die() {
         isAlive = false;
     }
 
-    public boolean intersects(BaseObject o){
+    /**
+     * Check whether the passed object (o) and the object (this) intersect.
+     */
+    public boolean intersects(BaseObject o) {
         double dx = x - o.x;
         double dy = y - o.y;
         double distance = Math.sqrt(dx * dx + dy * dy);
